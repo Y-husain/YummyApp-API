6b4971c95a151852dc289f186b0feff4d3c9d60f
Yahya <husain.host@gmail.com>
2018-04-18 20:10:24 +0300

    Modified decorator password

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,32 +1,30 @@
 from app import db
+from werkzeug.security import generate_password_hash
 
-class Bucketlist(db.Model):
-    """This class represents the bucketlist table."""
 
-    __tablename__ = 'bucketlists'
-
+class User(db.Model):
+    """Represent the user table"""
+    __tablename__ = "users_data"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(50), unique=True, index=True)
+    password_hash = db.Column(db.String(255))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(
-        db.DateTime, default=db.func.current_timestamp(),
-        onupdate=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
-    def __init__(self, name):
-        """initialize with name."""
-        self.name = name
+    @property
+    def password(self):
+        return AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password = generate_password_hash(password)
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
-    @staticmethod
-    def get_all():
-        return Bucketlist.query.all()
-
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-
-    def __repr__(self):
-        return "<Bucketlist: {}>".format(self.name)
