f958c72faf127fc7d699142caaf573d0379d9767
Yahya <husain.host@gmail.com>
2018-04-18 20:08:18 +0300

    Modified app

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,30 +1,32 @@
 from app import db
-from werkzeug.security import generate_password_hash
 
+class Bucketlist(db.Model):
+    """This class represents the bucketlist table."""
 
-class User(db.Model):
-    """Represent the user table"""
-    __table__ = "users_data"
+    __tablename__ = 'bucketlists'
+
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.string(100), nullable=False)
-    last_name = db.Column(db.string(100), nullable=False)
-    email = db.Column(db.string(50), unique=True, index=True)
-    password = db.Column(db.string(255))
+    name = db.Column(db.String(255))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func_current_timestamp(), onupdate= db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
 
-    @password.setter
-    def password(self, password):
-        self.password = generate_password_hash(password)
+    def __init__(self, name):
+        """initialize with name."""
+        self.name = name
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
+    @staticmethod
+    def get_all():
+        return Bucketlist.query.all()
+
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
-
-
-
+    def __repr__(self):
+        return "<Bucketlist: {}>".format(self.name)
