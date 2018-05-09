0cc8196b5e8ee14e961bf12981727f86f7bd6d7d
Yahya <husain.host@gmail.com>
2018-05-09 20:13:29 +0300

    Updated migration script

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 from app.models.user_model import User
+from app.models.category import Category
+from app.models.Recipe import Recipe
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
@@ -12,4 +14,4 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
-    db.create_all()
+    db.create_all() 
